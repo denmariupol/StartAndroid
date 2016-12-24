e773b23bfff5279e84a7ac56048492a47b562657
denmariupol <denmariupol@mail.ru>
2016-12-25 00:19:41 +0300

    lesson008

--- lesson008_working_with_code/src/main/java/com/example/lesson008_working_with_code/MainActivity.java
+++ lesson008_working_with_code/src/main/java/com/example/lesson008_working_with_code/MainActivity.java
@@ -2,12 +2,25 @@ package com.example.lesson008_working_with_code;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main);
+        TextView myView = (TextView) findViewById(R.id.textView);
+        myView.setText("new text");
+
+        Button myBtn = (Button)findViewById(R.id.button);
+        myBtn.setText("!!!!!!");
+        myBtn.setEnabled(false);
+
+        CheckBox myChk = (CheckBox) findViewById(R.id.checkBox);
+        myChk.setChecked(true);
     }
 }
