f8449811e8179ec5705cdd16f8eaab55445837e0
denmariupol <denmariupol@mail.ru>
2016-12-26 20:21:51 +0300

    lesson017

---
+++ lesson014_menu_groups_order_menuinflator_xmlmenu/src/main/java/com/example/lesson014_menu_groups_order_menuinflator_xmlmenu/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.lesson014_menu_groups_order_menuinflator_xmlmenu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
